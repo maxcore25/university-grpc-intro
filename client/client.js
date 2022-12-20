e0f68de1448100da7eb76292eb48a4c914d19c72
Max <mkorobov2002@mail.ru>
2022-12-20 17:09:14 +0300

    Write working version

---
+++ client/client.js
@@ -0,0 +1,20 @@
+const PROTO_PATH = '../proto/product.proto';
+
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  arrays: true,
+});
+
+const CustomerService =
+  grpc.loadPackageDefinition(packageDefinition).CustomerService;
+const client = new CustomerService(
+  'localhost:30043',
+  grpc.credentials.createInsecure()
+);
+
+module.exports = client;
